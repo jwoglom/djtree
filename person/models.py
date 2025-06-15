51e8b3dcd4c6945d77c3caf1c5670a97e56b2ba7
James Woglom <j@wogloms.net>
2025-06-15 17:04:35 -0400

    add event models

--- person/models.py
+++ person/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Person(models.Model):
@@ -11,7 +12,7 @@ class Person(models.Model):
     def __str__(self):
         return f"Person: {self.name}"
 
-
+# Names
 class Name(models.Model):
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100, blank=True)
@@ -31,4 +32,114 @@ class PersonName(models.Model):
 
     def __str__(self):
         return f"{self.name}{f' ({self.name_type})' if self.name_type else ''}"
+
+# Events
+
+class Event(models.Model):
+    date = models.DateField()
+    person = models.ForeignKey('Person', on_delete=models.CASCADE, related_name='events')
+    comment = models.TextField(blank=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"{self.__class__.__name__.replace('Event', '')} event for {self.person} on {self.date}"
+
+class CoupleEvent(Event):
+    other_person = models.ForeignKey('Person', on_delete=models.CASCADE, related_name='couple_events_as_other')
+    location = models.CharField(max_length=200, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        is_new = self.pk is None
+        super().save(*args, **kwargs)
+        
+        # Find or create the symmetric event
+        symmetric_event, created = self.__class__.objects.get_or_create(
+            person=self.other_person,
+            other_person=self.person,
+            date=self.date,
+            defaults={
+                'location': self.location,
+                'comment': self.comment
+            }
+        )
+        
+        # If this is an update (not new) and the symmetric event exists,
+        # update its fields to match this one
+        if not is_new and not created:
+            symmetric_event.location = self.location
+            symmetric_event.comment = self.comment
+            symmetric_event.date = self.date
+            symmetric_event.save(update_fields=['location', 'comment', 'date'])
+
+class MarriageEvent(CoupleEvent):
+    ended = models.BooleanField(default=False)  # Track if this marriage ended in divorce
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['person', 'other_person', 'date'],
+                name='unique_marriage_per_couple_date'
+            )
+        ]
+
+class DivorceEvent(CoupleEvent):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['person', 'other_person', 'date'],
+                name='unique_divorce_per_couple_date'
+            )
+        ]
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        MarriageEvent.objects.filter(
+            person=self.person,
+            other_person=self.other_person,
+            ended=False
+        ).update(ended=True)
+        
+        MarriageEvent.objects.filter(
+            person=self.other_person,
+            other_person=self.person,
+            ended=False
+        ).update(ended=True)
+
+class BirthEvent(Event):
+    location = models.CharField(max_length=200, blank=True)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['person'],
+                name='unique_birth_per_person'
+            )
+        ]
+
+class DeathEvent(Event):
+    location = models.CharField(max_length=200, blank=True)
+    cause = models.CharField(max_length=200, blank=True)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['person'],
+                name='unique_death_per_person'
+            )
+        ]
+
+class ImmigrationEvent(Event):
+    from_country = models.CharField(max_length=100)
+    to_country = models.CharField(max_length=100)
+    location = models.CharField(max_length=200, blank=True)
+
+class CitizenshipEvent(Event):
+    country = models.CharField(max_length=100)
+    location = models.CharField(max_length=200, blank=True)
 
