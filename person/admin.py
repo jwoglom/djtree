13c8d4412f251bd23d693cdc5b4a885d9361a9a8
James Woglom <j@wogloms.net>
2025-06-15 16:45:44 -0400

    tweak admin ui

--- person/admin.py
+++ person/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Person, Name, PersonName
 
-# Register your models here.
+class PersonNameInline(admin.TabularInline):
+    model = PersonName
+    extra = 1
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [PersonNameInline]
+
+class NameAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        """
+        Return a dict of all perms for this model. This dict has the keys
+        add, change, delete, and view mapping to the True/False for each of those actions.
+        """
+        return {}
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Name, NameAdmin)
+
