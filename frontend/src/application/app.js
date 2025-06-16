d49d47be4ddb57571ef3a86d4a29f64d8e20f1a6
James Woglom <j@wogloms.net>
2025-06-15 22:16:29 -0400

    react boilerblate

--- frontend/src/application/app.js
+++ frontend/src/application/app.js
@@ -1,16 +1,7 @@
+import React from "react";
 
-// This is the style entry file
-import "../styles/index.scss";
+const App = () => {
+  return <div>Hello world</div>;
+};
 
-// We can import other JS file as we like
-import Jumbotron from "../components/jumbotron";
-
-window.document.addEventListener("DOMContentLoaded", function () {
-  window.console.log("dom ready");
-
-  // Find elements and initialize
-  for (const elem of document.querySelectorAll(Jumbotron.selector())) {
-    new Jumbotron(elem);
-  }
-});
-
+export default App;
---
+++ frontend/src/application/index.js
@@ -0,0 +1,7 @@
+import React from "react"; // eslint-disable-line no-unused-vars
+import { createRoot } from "react-dom/client";
+import App from "./app";
+
+const root = document.getElementById('root');
+const rootElement = createRoot(root);
+rootElement.render(<App />);
