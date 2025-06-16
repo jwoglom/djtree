c91e319a3385df6e3c62e68880d6e46df12b422d
James Woglom <j@wogloms.net>
2025-06-15 20:58:48 -0400

    event inline

--- person/admin.py
+++ person/admin.py
@@ -1,12 +1,71 @@
 from django.contrib import admin
-from .models import Person, Name, PersonName
+from django import forms
+from .models import (
+    Person, Name, PersonName,
+    BirthEvent, DeathEvent, MarriageEvent, DivorceEvent,
+    ImmigrationEvent, CitizenshipEvent
+)
+
+class EventForm(forms.ModelForm):
+    comment = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 2, 'cols': 40}),
+        required=False
+    )
 
 class PersonNameInline(admin.TabularInline):
     model = PersonName
     extra = 1
 
+class BirthEventInline(admin.TabularInline):
+    model = BirthEvent
+    form = EventForm
+    extra = 0
+    fields = ('date', 'location', 'comment')
+
+class DeathEventInline(admin.TabularInline):
+    model = DeathEvent
+    form = EventForm
+    extra = 0
+    fields = ('date', 'location', 'cause', 'comment')
+
+class MarriageEventInline(admin.TabularInline):
+    model = MarriageEvent
+    form = EventForm
+    extra = 0
+    fields = ('date', 'other_person', 'location', 'comment', 'ended')
+    fk_name = 'person'
+
+class DivorceEventInline(admin.TabularInline):
+    model = DivorceEvent
+    form = EventForm
+    extra = 0
+    fields = ('date', 'other_person', 'location', 'comment')
+    fk_name = 'person'
+
+class ImmigrationEventInline(admin.TabularInline):
+    model = ImmigrationEvent
+    form = EventForm
+    extra = 0
+    fields = ('date', 'from_country', 'to_country', 'location', 'comment')
+
+class CitizenshipEventInline(admin.TabularInline):
+    model = CitizenshipEvent
+    form = EventForm
+    extra = 0
+    fields = ('date', 'country', 'location', 'comment')
+
 class PersonAdmin(admin.ModelAdmin):
-    inlines = [PersonNameInline]
+    inlines = [
+        PersonNameInline,
+        BirthEventInline,
+        DeathEventInline,
+        MarriageEventInline,
+        DivorceEventInline,
+        ImmigrationEventInline,
+        CitizenshipEventInline,
+    ]
+    list_display = ('__str__',)
+    search_fields = ('names__first_name', 'names__last_name')
 
 class NameAdmin(admin.ModelAdmin):
     def get_model_perms(self, request):
@@ -16,6 +75,7 @@ class NameAdmin(admin.ModelAdmin):
         """
         return {}
 
+# Register models
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Name, NameAdmin)
 
